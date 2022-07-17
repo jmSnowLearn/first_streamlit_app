7f698a18ebaa1ff2113bd56baf5c09324f8df11e
JM Snowflake Learner <109476586+jmSnowLearn@users.noreply.github.com>
2022-07-17 06:49:07 -1000

    Update streamlit_app.py
    python is case sensitive... fixed the spelling error

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.subheader('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-Streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
